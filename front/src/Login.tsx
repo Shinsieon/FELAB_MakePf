7b51715eb93130ed85ce6a2317f7c1109e05a2b7
Shin Sie On <coolguysiun@gmail.com>
2023-05-03 20:52:27 +0900

    change color

--- front/src/Login.tsx
+++ front/src/Login.tsx
@@ -85,7 +85,7 @@ function Login() {
                       Password
                     </label>
                     <input
-                      className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-600 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                       id="password"
                       type="password"
                       placeholder="******************"
