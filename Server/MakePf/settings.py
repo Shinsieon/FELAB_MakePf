b5cdacc3ea414d9cba91501228386e64b38e3b7c
Shin Sie On <coolguysiun@gmail.com>
2023-05-07 23:01:35 +0900

    pymysql for ubuntu

--- Server/MakePf/settings.py
+++ Server/MakePf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import datetime, timedelta
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,7 +101,7 @@ WSGI_APPLICATION = 'MakePf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
