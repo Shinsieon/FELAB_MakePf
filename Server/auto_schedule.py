b891bfcb91f9a4a3f3533728a0e8e258d4054c60
Shin Sie On <coolguysiun@gmail.com>
2023-05-23 21:50:43 +0900

    python data crawling

--- Server/auto_schedule.py
+++ Server/auto_schedule.py
@@ -1,26 +1,51 @@
 import schedule
-import time
 #특정 함수 정의
 import pymysql
-from datetime import datetime, timedelta
-#import FinanceDataReader as fdr
-import requests
-import pandas as pd
-from io import BytesIO
-import numpy as np
 from datetime import datetime
+#import FinanceDataReader as 
+from pykrx import stock 
+import pandas as pd
+import numpy as np
 import time
 from sqlalchemy import create_engine 
-import sys, traceback
-import logging
+import FinanceDataReader as fdr
+
+class dbConn:
+    def startConnection(self):
+        self.conn = pymysql.connect(host='localhost', user='root', password='su970728!', db='foliogram')
+        self.engine= create_engine('mysql+pymysql://root:su970728!@localhost:3306/foliogram') #pymysql로 작성시 error
+        self.curs = self.conn.cursor()
+        print(self.curs)
 
 class autoUpdate:
-    def startConnection(self):
-        self.conn = pymysql.connect(host='localhost', user='root', password='su970728!', db='PORTFOLIO')
-        self.engine= create_engine('mysql+pymysql://root:su970728!@localhost:3306/PORTFOLIO') #pymysql로 작성시 error
-        self.curs = self.conn.cursor()
-    def getOhlcvWithReturn(self,code):
-        today = datetime.today().strftime("%Y%m%d") 
-        df = stock.get_market_ohlcv("20130101", today, code,"m") #월별 시고저종+ 거래량
+    def __init__(self):
+        self.startDate =  "20130101"#10년전
+        self.endDate = datetime.today().strftime("%Y%m%d") 
+
+    
+    def getListOfKospi(self):
+        return stock.get_market_ticker_list()
+        
+    def getOhlcvWithReturn(self):
+        df = stock.get_market_ohlcv(self.startDate, self.endDate, "005930","m") #월별 시고저종+ 거래량
+        print(df)
+    def getKospiClose(self):
+        result_df = pd.DataFrame()
+        process_num = 0
+        stnd_date = "2013-01-01"
+        end_date = datetime.today().strftime("%Y%m%d")
+        stock_list = stock.get_market_ticker_list()
+        stock_list_len = len(stock_list)
+
+        for code in stock_list:
+            df = fdr.DataReader(code, stnd_date, end_date)['Close']
+            df = pd.DataFrame(df.tolist(), index=df.index, columns=[code])
+            result_df = pd.concat([result_df, df],join='outer', axis=1)
+            process_num+=1
+            time.sleep(1)
+            print(str(process_num) + "/ " + str(stock_list_len))
+            if(process_num == 10):
+                break
+            
 db = autoUpdate()
-db.startConnection()
+db.getOhlcvWithReturn()
